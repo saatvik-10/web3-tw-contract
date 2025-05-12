83dee130390e91a00e68046e0655225f67deaa58
saatvik-10 <saatvikmadan529@gmail.com>
2025-05-13 01:14:13 +0530

    added struct for tweet

--- contracts/_Tw.sol
+++ contracts/_Tw.sol
@@ -2,17 +2,31 @@
 pragma solidity ^0.8.0;
 
 contract Twitter {
-    mapping(address => string[]) public tweets;
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+    }
+
+    mapping(address =>  Tweet[]) public tweets;
 
     function createTweet(string memory _tweet) public {
-        tweets[msg.sender].push(_tweet);
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweet);
     }
 
-    function getTweet(address _owner, uint _i) public view returns (string memory) {
+    function getTweet(address _owner, uint _i) public view returns (Tweet memory) {
         return tweets[_owner][_i];
     }
-
-    function getAllTweets(address _owner) public view returns(string[] memory) {
+ 
+    function getAllTweets(address _owner) public view returns(Tweet[] memory) {
         return tweets[_owner];
     }
 }
